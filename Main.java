c5f95d79e902a3e420e9aa0c67b54f7bdaccef1b
Shakti Rajput <33245057+shakti-rajput@users.noreply.github.com>
2021-02-07 04:41:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,8 +12,8 @@ class Movie
    private String name;
    private String year;
    private String gener;
-   private int rating;
-   private int raters;
+   private int rating=0;
+   private int raters=0;
    
 //   private List<User> userList=new ArrayList<User>();
    private Map<String, Integer> userList  = new HashMap<String, Integer>();
@@ -33,6 +33,16 @@ class Movie
     public String getYear()
     {
         return this.year;
+    }
+    public void addUser(String user,int rating)
+    {
+        this.userList.put(user,rating);
+        this.rating=(this.rating*raters+rating)/(raters+1);
+        this.raters+=1;
+    }
+    public int getRating()
+    {
+        return this.rating;
     }
     public Boolean checkUser(String user)
     {
@@ -56,24 +66,28 @@ class User
 {
     // Instance Variables
     private String name;
-    private String category;
+    private String category="";
     private int reviewCount;
     // Constructor Declaration of Class
     public User(String name)
     {
         this.name = name;
     }
-    public String iscategory(String name)
+    public String getname()
+    {
+        return this.name;
+    }
+    public String iscategory()
     {
         return this.category;
     }
-    public int totalReview(String name)
+    public int totalReview()
     {
         return this.reviewCount;
     }
-    public void increaseReviewCount(String name)
+    public void increaseReviewCount()
     {
-         this.reviewCount++;
+         this.reviewCount+=1;
          if(reviewCount==3)
          this.category="critic";
     }
@@ -82,10 +96,39 @@ class ReviewSystem
 {
     // Instance Variables
     List<Movie> movieList=new ArrayList<Movie>();
-    List<User> UserList=new ArrayList<User>();
+    List<User> users=new ArrayList<User>();
     public void addReview(String person,String movie, int rating)
     {
-         if(Collections.binarySearch(movieList))
+        //     Comparator<Movie> c = new Comparator<Movie>() {
+        //   public int compare(Movie u1, Movie u2) {
+        //     return u1.getname().compareTo(u2.getname());
+        //   }
+        // };
+        int movieIndex = Collections.binarySearch(movieList, new Movie(movie,"",""), Comparator.comparing(Movie::getname));
+        int userIndex = Collections.binarySearch(users, new User(person), Comparator.comparing(User::getname));
+         
+         if((movieIndex>=0 && movieIndex<movieList.size())  && (userIndex>=0 && userIndex<users.size()) )
+         {
+             if(!movieList.get(movieIndex).checkUser(person))
+             {
+                 users.get(userIndex).increaseReviewCount();
+                 if(users.get(userIndex).iscategory().equals("critic"))
+                 {
+                     rating=rating*2;
+                 }
+                 movieList.get(movieIndex).addUser(person,rating);
+                 
+                //  System.out.println("Raja");
+             }
+             else
+             {
+                 System.out.println(person +" User already added rating for "+movie+" movie");
+             }
+         }
+         else
+         {
+             System.out.println("Either Movie or User was not in list");
+         }
     }
     // Constructor Declaration of Class
     
@@ -96,22 +139,47 @@ public class Main
   
 	public static void main(String[] args) {
 	    
-		System.out.println("Hello World");
+// 		System.out.println("Hello World");
 		Movie x=new Movie("RajaHinustani","2000","Romantic");
 		Movie y=new Movie("Hinustani","2000","Romantic");
 		Movie z=new Movie("Raa","2000","Romantic");
+		Movie a=new Movie("Raa2","2000","Romantic");
 		
-		ReviewSystem review=new ReviewSystem();
-		review.movieList.add(x);
-		review.movieList.add(y);
-		review.movieList.add(z);
-        review.movieList.sort(Comparator.comparing(Movie::getname));
-// 		Collections.sort(review.movieList, new CompareObj());
+		ReviewSystem system=new ReviewSystem();
+		system.movieList.add(x);
+		system.movieList.add(y);
+		system.movieList.add(z);
+		system.movieList.add(a);
+        system.movieList.sort(Comparator.comparing(Movie::getname));  // sort for indexing on columns
+        
+        
+// 		Collections.sort(system.movieList, new CompareObj());
 // 		List<Movie> movieList=new ArrayList<Movie>();
         // TreeSet<String> movieList = new TreeSet<String>();
 // 		 movieList.add(x);
+
 		 User sharuk = new User("Sharuk Khan");
-		 for (int i = 0; i < review.movieList.size(); i++) 
-            System.out.print(review.movieList.get(i).getname() + " ");
+		 User salman = new User("Salman Khan");
+		 User amir = new User("Amir Khan");
+		 system.users.add(sharuk);
+		 system.users.add(salman);
+		 system.users.add(amir);
+		 system.users.sort(Comparator.comparing(User::getname)); // sort for indexing on columns
+		 
+
+		 system.addReview("Sharuk Khan","Raa",2);
+		 system.addReview("Sharuk Khan","Raa",5); // Same movie cannot be reviewed twice by same person
+		 
+		 system.addReview("Sharuk Khan","RajaHinustani",1);
+		 system.addReview("Sharuk Khan","Hinustani",5);
+		 system.addReview("Salman Khan","Hinustani",3);
+		 system.addReview("Amir Khan","Hinustani",2);
+		 
+		 
+		 system.addReview("Sharuk Khan","Raa2",3); //Sharuk becomes critic
+		 
+		  // System.out.println(system.users.get(2).totalReview());
+		 for (int i = 0; i < system.movieList.size(); i++) 
+            System.out.println(system.movieList.get(i).getname() + " "+system.movieList.get(i).getRating());
 	}
 }
